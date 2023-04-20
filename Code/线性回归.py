3b1d9f803aff03bb7218efb22a538585b092a020
helei <hl238683@163.com>
2023-04-20 17:54:33 +0800

    线性回归

--- Code/线性回归.py
+++ Code/线性回归.py
@@ -19,8 +19,7 @@ print(features[0], labels[0])
 
 
 def use_svg_display():
-    matplotlib_inline.backend_inline.set_matplotlib_formats()
-    # display.set_matplotlib_formats('svg')
+    matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
 
 
 def set_figsize(figsize=(3.5, 2.5)):
@@ -28,5 +27,69 @@ def set_figsize(figsize=(3.5, 2.5)):
     plt.rcParams['figure.figsize'] = figsize
 
 
-set_figsize()
-plt.scatter(features[:, 1].asnumpy(), labels.asnumpy(), 1);
+# set_figsize()
+# plt.scatter(features[:, 1].asnumpy(), labels.asnumpy(), 1);
+
+
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        j = nd.array(indices[i: min(i + batch_size, num_examples)])
+        yield features.take(j), labels.take(j)
+
+
+batch_size = 10
+for x, y in data_iter(batch_size, features, labels):
+    print(x, y)
+    break
+w = nd.random.normal(scale=0.01, shape=(num_inputs, 1))
+b = nd.zeros(shape=(1,))
+w.attach_grad()
+b.attach_grad()
+
+"""
+定义模型
+"""
+
+
+def linreg(x, w, b):
+    return nd.dot(x, w) + b
+
+
+"""
+定义损失函数
+"""
+
+
+def squared_loss(y_hat, y):
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+
+"""
+定义优化算法
+"""
+
+
+def sgd(params, lr, batch_size):
+    for param in params:
+        param[:] = param - lr * param.grad / batch_size
+
+
+"""
+训练模型
+"""
+
+lr = 0.03
+num_epochs = 3
+net = linreg
+loss = squared_loss
+for epoch in range(num_epochs):
+    for x, y in data_iter(batch_size, features, labels):
+        with autograd.record():
+            l = loss(net(x, w, b), y)
+        l.backward()
+        sgd([w, b], lr, batch_size)
+    train_l = loss(net(features, w, b), labels)
+    print('epoch%d, loss%f' % (epoch + 1, train_l.mean().asnumpy()))

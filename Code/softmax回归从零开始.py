207d806be5e2201031ee72a332a5049c5b190fd2
helei <hl238683@163.com>
2023-04-21 10:42:07 +0800

    softmax回归从零开始

--- Code/softmax回归从零开始.py
+++ Code/softmax回归从零开始.py
@@ -3,7 +3,7 @@ import d2lzh as d2l
 from mxnet import autograd, nd
 
 batch_size = 256
-train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)  # 制作数据集 默认的数据就是fashion-mnist
 #  输入样本数， 输出种类
 num_inputs = 784
 num_outputs = 10
@@ -26,7 +26,7 @@ def softmax(x):
     这保证了矩阵每行元素和为1,且不为负
     """
     x_exp = x.exp()
-    partition = x.exp.sum(axis=1, keepdims=True)
+    partition = x_exp.sum(axis=1, keepdims=True)
     return x_exp / partition
 
 
@@ -82,7 +82,7 @@ def evaluate_accuracy(data_iter, net):
     return acc_sum / n
 
 
-num_epoch, lr = 5, 0.1
+num_epochs, lr = 5, 0.1
 
 
 def tain_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params=None, lr=None, trainer=None):
@@ -93,10 +93,29 @@ def tain_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params=No
     :param loss:
     :param num_epochs:
     :param batch_size:
-    :param params:
+    :param params:初始化的w,b值
     :param lr:
     :param trainer:
     :return:
     """
     for epoch in range(num_epochs):
-        pass
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for x, y in train_iter:
+            with autograd.record():
+                y_hat = net(x)
+                l = loss(y_hat, y).sum()
+            l.backward()
+            if trainer is None:
+                d2l.sgd(params, lr, batch_size)
+            else:
+                trainer.step(batch_size)
+            y = y.astype('float32')
+            train_l_sum += l.asscalar()
+            train_acc_sum += (y_hat.argmax(axis=1) == y).sum().asscalar()  # asscalar()将向量转成标量
+            n += y.size
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train_acc_sum %.3f, test_acc %.3f' % (epoch + 1,
+                                                                          train_l_sum / n, train_acc_sum / n, test_acc))
+
+
+tain_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, batch_size, [w, b], lr)

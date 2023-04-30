32a245b4ad948c2421e02b99362704b98e2ab36a
贺磊 <hl238683@163.com>
2023-04-30 10:10:29 +0800

    构造更复杂的模型，使用get_constant函数创建的随机权重参数不会在训练中被迭代

--- Code/继承Block构建自定义模型.py
+++ Code/继承Block构建自定义模型.py
@@ -33,7 +33,7 @@ Block类是一个通用的部件， Sequential类继承Block类。
 
 """
 
-
+#  使用Sequential思想，通过add函数构建自定义模型
 class MySequential(nn.Block):
     def __init__(self):
         super(MySequential, self).__init__()
@@ -53,3 +53,34 @@ net.add(nn.Dense(256, activation='relu'))  # 添加Block实例
 net.add(nn.Dense(10))  # 添加Block实例
 net.initialize()
 print(net(x))
+
+"""
+构造更加复杂的模型
+"""
+class FancyMLP(nn.Block):
+    def __init__(self):
+        super(FancyMLP, self).__init__()
+        # 使用get_constant函数创建的随机权重参数不会在训练中被迭代（即常参数）
+        self.rand_weight = self.params.get_constant('rand_weight', nd.random.uniform(shape=(20, 20)))
+        self.dense = nn.Dense(20, activation='relu')
+
+    def forward(self, x):
+        x = self.dense(x)
+        # 使用创建的常参数，以及NDArray的relu函数和dot函数
+        x = nd.relu(nd.dot(x, self.rand_weight.data()) + 1)
+        # 复用全连接层，等于两个全连接层共享参数
+        x = self.dense(x)
+        # 控制流，这里使用asscalar()返回标量进行比较
+        while x.norm().asscalar() > 1:
+            x /= 2
+        if x.norm().asscalar()<0.8:
+            x *=10
+        return x.sum()
+
+net = FancyMLP()
+net.initialize()
+print(net(x))
+
+
+
+

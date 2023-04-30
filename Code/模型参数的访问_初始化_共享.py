699b785b38e7822f774b9db31e8fc3ee4e303692
贺磊 <hl238683@163.com>
2023-04-30 12:34:46 +0800

    初始化模型参数.py

--- Code/模型参数的访问_初始化_共享.py
+++ Code/模型参数的访问_初始化_共享.py
@@ -21,10 +21,51 @@ y = net(x)
 访问模型参数
 
 """
-
+# net[0].params 获取第一层网络的权重以及偏置的的信息
 print('第一个输出：',net[0].params, '第一个的第二个输出：',type(net[0].params))
+# 这两种方式获取的都是网络第一层的权重。
+# Parameter dense0_weight (shape=(256, 20), dtype=float32)
 print('第二个输出：',net[0].params['dense0_weight'],'第二个的第二个输出', net[0].weight)
+# 输出第一层网络权重的具体数值，偏置的具体数值。
+print('第三个输出：',net[0].weight.data(), net[0].bias.data())
 
-print('第三个输出：',net[0].weight.data())
+
+# 会输出所有层的权重偏置信息
+"""
+通过collect_params()函数
+来获取net变量所有嵌套的层所包含的所有参数。
+会输出所有层的权重偏置信息
+sequential0_ (
+  Parameter dense0_weight (shape=(256, 20), dtype=float32)
+  Parameter dense0_bias (shape=(256,), dtype=float32)
+  Parameter dense1_weight (shape=(10, 256), dtype=float32)
+  Parameter dense1_bias (shape=(10,), dtype=float32)
+)
+"""
+print(net.collect_params())
+"""
+collect_params()函数可以通过正则表达式来匹配参数名，
+从而筛选出需要的参数
+"""
+print(net.collect_params('.*weight'))
+
+""""
+初始化模型参数
+"""
+# 例子 我们将权重参数初始化成均值为0,
+# 标准差为0.01的正态分布随机数， 并依然将偏差参数清零。
+net.initialize(init=init.Normal(sigma=0.01), force_reinit=True)
+print(net[0].weight.data()[0])
+
+
+net.initialize(init=init.Constant(1), force_reinit=True)
+print(net[0].weight.data()[0])
+
+
+
+
+
+
+
 
 
